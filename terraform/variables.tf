de7e0b35e2172e47deb346a04cf70e1f36bf7782
rwilliamspbg-ops <221235059+rwilliamspbg-ops@users.noreply.github.com>
2026-02-19 09:35:50 -0800

    Surgical fix: Restore variable declarations only

---
+++ terraform/variables.tf
@@ -0,0 +1,4 @@
+variable "aws_region" { default = "us-east-1" }
+variable "node_count" { default = 8 }
+variable "key_pair_name" { default = "sovereign-fl-key" }
+variable "iam_role" { default = "SovereignFL-EC2-Role" }
