368927d1329edb3b08dd3d4fa0ed5a65806c07d9
Ryan <221235059+rwilliamspbg-ops@users.noreply.github.com>
2026-02-20 08:22:28 -0800

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,23 @@
-variable "aws_region" { default = "us-east-1" }
-variable "node_count" { default = 8 }
-variable "key_pair_name" { default = "sovereign-fl-key" }
-variable "iam_role" { default = "SovereignFL-EC2-Role" }
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "node_count" {
+  description = "Number of worker nodes"
+  type        = number
+  default     = 1 # Scaled down from 100 for testing
+}
+
+variable "key_pair_name" {
+  description = "Name of the SSH key pair"
+  type        = string
+  default     = "sovereign-fl-key"
+}
+
+variable "iam_role" {
+  description = "IAM role name for EC2"
+  type        = string
+  default     = "SovereignFL-EC2-Role"
+}
